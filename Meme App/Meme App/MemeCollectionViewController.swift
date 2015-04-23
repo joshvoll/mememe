b9d81ca0d3c89349ed88bc97b33443c2e05dbd90
josue <joshvoll@yahoo.com>
2015-04-23 16:59:38 -0600

    Committed by Udacity

---
+++ Meme App/Meme App/MemeCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCollectionViewControllerCell.swift
+//  Meme App
+//
+//  Created by Josue Rodriguez on 4/20/15.
+//  Copyright (c) 2015 Josue Rodriguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewControllerCell:UICollectionViewCell{
+    
+    var meme:Meme?
+    
+    
+}
